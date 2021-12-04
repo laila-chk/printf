3f805ecd3318798ab6488430412d7dfe7529f505
Laila Chokri <lchokri@e2r8p10.1337.ma>
2021-12-04 03:52:52 +0100

    2nd oush

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:45:56 by lchokri           #+#    #+#             */
-/*   Updated: 2021/12/03 18:44:34 by lchokri          ###   ########.fr       */
+/*   Updated: 2021/12/04 03:01:15 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,21 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+char *ft_toupper(char *s, char *flag)
+{
+	int i;
+
+	i = 0;
+	if (*flag == 'X')
+		while (s[i])
+		{
+			if (s[i] <= 122 && s[i] >= 97)
+				s[i] -= 32;
+			i++;
+		}
+	return (s);
 }
 
 void	ft_putstr(char *str)
@@ -32,30 +47,69 @@ void	ft_putstr(char *str)
 	}
 }
 
-static void	tobase(unsigned int num, int base)
+void	ft_bzero(void *s, size_t n)
 {
-	char mabase[] = "0123456789abcdef";
-	char maholder[50];
-	char *p;
+	while (n > 0)
+	{
+		*(char *)s = '\0';
+		s++;
+		n--;
+	}
+}
 
-	p = &maholder[50];
-	*p = '\0';
+void   *ft_calloc(size_t count, size_t size)
+{
+        void    *p;
+
+        p = malloc(count * size);
+        if (!p)
+                return (NULL);
+        ft_bzero(p, size * count);
+        return (p);
+}
+
+char	*tobase(unsigned long num, int base)
+{
+	char		*maholder;
+	char		*p;
+	unsigned long y;
+	char		*mabase;
+	int			i;
+
+	i = 0;
+	mabase = "0123456789abcdef";
+	y = num;
+	while (y)
+	{
+		y /= base;
+		i++;
+	}
+	maholder = (char *)ft_calloc(i + 1, sizeof(char));
+	p = &maholder[y];
 	while (num)
 	{
 		p--;
 		*p = mabase[num % base];
 		num /= base;
 	}
-	if (base == 16)
-	{
-		 p--;
-		*p = 'x';
-		 p--;
-		*p = '0';
-	}
-	ft_putstr(p);
+	return (p);
 }
 
+void	putptr(unsigned long n)
+{
+	write(1, "0x", 2);
+	ft_putstr(tobase(n, 16));
+}
+
+void	ft_writenbr(long n)
+{
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -1 * n;
+	}
+	ft_putstr(tobase(n, 10));
+}
 int ft_printf(const char* str, ...)
 {
 	va_list args;
@@ -65,28 +119,25 @@ int ft_printf(const char* str, ...)
 	s = str;
 	while(*s != '\0')
 	{
-		while (*s != '%' && *s != '\0')
-		{
-			ft_putchar(*s);
-			s++;
-		}
-		if (*s == '%')
-			s++;
-		if (*s == 'c')
-		{
+		if (*s == '%' && *(s + 1) == 'c')
 			ft_putchar(va_arg(args, int));
-			s++;
-		}
-		else if (*s == 's')
-		{
+		else if (*s == '%' && *(s + 1) == 's')
 			ft_putstr(va_arg(args, char *));
+		else if (*s == '%' && *(s + 1) == 'p')
+			putptr(va_arg(args, unsigned long));
+		else if(*s == '%' && (*(s + 1) == 'x' || *(s + 1) == 'X'))
+			ft_putstr(ft_toupper(tobase(
+			va_arg(args, unsigned long), 16), (char *)s + 1));
+		else if (*s == '%' && *(s + 1) == 'u')
+			ft_putstr(tobase(va_arg(args, unsigned long), 10));
+		else if (*s == '%' && (*(s + 1) == 'd' || *(s + 1) == 'i'))
+			ft_writenbr(va_arg(args, int));
+		else
+			ft_putchar(*s);
+		if (*s != '%')
 			s++;
-		}
-		else if (*s == 'p')
-		{
-			tobase(va_arg(args, int), 16);
-			s++;
-		}
+		else if (*s == '%')
+			s += 2;
 	}
 	va_end(args);
 	return (1);
@@ -98,7 +149,20 @@ int main()
 	char *p;
 	p = &g;
 	char bro[] = "yalahwi";
-	ft_printf("%c \n",g);
-	ft_printf("%s\n", bro);
-	ft_printf("%p \n",p);
+	ft_printf("%c %a jds >>>>>>",g);
+	printf("%c \n",g);
+	ft_printf("%p >>>>>>", &g);
+	printf("%p\n", &g);
+	ft_printf("%s >>>>>>", bro);
+	printf("%s\n", bro);
+	ft_printf("%X >>>>>",500);
+	printf("%X\n",500);
+	ft_printf("%x >>>>>",500);
+	ft_printf("%x\n",500);
+	ft_printf("%u >>>>>", -1);
+	ft_printf("%u\n", -1);
+	ft_printf("%d >>>>>",2147483647);
+	printf("%d\n",2147483647);
+	ft_printf("%lu >>>>>", 5);
+	printf("%lu\n",5);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 17:13:44 by lchokri           #+#    #+#             */
-/*   Updated: 2021/12/03 17:24:14 by lchokri          ###   ########.fr       */
+/*   Updated: 2021/12/03 22:31:10 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,5 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-//void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putnbr(int nb);
-int		ft_toupper(int c);
 
 #endif
